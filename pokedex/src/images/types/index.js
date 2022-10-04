6610251c979d74860ba2eafc9e8ca3000ba67459
PedroSehn <pedrorsehn@hotmail.com>
2022-10-03 21:42:53 -0300

    adiciona pastas imagens de tipo e fonte

---
+++ pokedex/src/images/types/index.js
@@ -0,0 +1,41 @@
+import bug from './BugIC_Big.png'
+import dark from './DarkIC_Big.png'
+import dragon from './DragonIC_Big.png'
+import electric from './ElectricIC_Big.png'
+import fairy from './FairyIC_Big.png'
+import fighting from './FightingIC_Big.png'
+import fire from './FireIC_Big.png'
+import flying from './FlyingIC_Big.png'
+import ghost from './GhostIC_Big.png'
+import grass from './GrassIC_Big.png'
+import ground from './GroundIC_Big.png'
+import ice from './IceIC_Big.png'
+import normal from './NormalIC_Big.png'
+import poison from './PoisonIC_Big.png'
+import psychic from './PsychicIC_Big.png'
+import rock from './RockIC_Big.png'
+import steel from './SteelIC_Big.png'
+import unknown from './UnknownIC_Big.png'
+import water from './WaterIC_Big.png'
+
+export const type = {
+    bug: bug,
+    dark: dark,
+    dragon: dragon,
+    electric: electric,
+    fairy: fairy,
+    fighting: fighting,
+    fire: fire,
+    flying: flying,
+    ghost: ghost,
+    grass: grass,
+    ground: ground,
+    ice: ice,
+    normal: normal,
+    poison: poison,
+    psychic: psychic,
+    rock: rock,
+    steel: steel,
+    unknown: unknown,
+    water: water,
+}
